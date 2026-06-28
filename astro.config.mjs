c63d9badc9a8a4932ce0d7e799817919fae3e9c7
arcjet-zz <dystopia321@gmail.com>
2026-06-28 22:19:21 +0800

    Configure custom Pages domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
-const [owner, repository] = (process.env.GITHUB_REPOSITORY ?? 'local/icltp-website-test').split('/');
-const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+const site = process.env.SITE_URL ?? 'http://localhost:4321';
 
 export default defineConfig({
-  site: isGitHubPages ? `https://${owner}.github.io` : 'http://localhost:4321',
-  base: isGitHubPages ? `/${repository}` : '/',
+  site,
+  base: '/',
   output: 'static',
   integrations: [preact()],
   devToolbar: {
